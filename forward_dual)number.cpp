5b16ea6179a1e121c614fd0e74148d98d5becc78
MCMCM <mincong.zhang@hotmail.com>
2023-09-27 21:01:00 +0100

    Create forward_dual)number.cpp

---
+++ forward_dual)number.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class Dual
+{
+    private:
+    double m_real; //Real part
+    double m_inf_simal; //Infinitesimal part
+    
+    public:
+    Dual (double r, double i = 0.0): m_real(r), m_inf_simal(i) {}
+
+    double real() const
+    {
+        return m_real;
+    }
+    
+    double infsimal() const
+    {
+        return m_inf_simal;
+    }
+    
+    double deriv() const
+    {
+        return m_inf_simal;
+    }
+};
+
+Dual operator+(const Dual &x, const Dual &y)
+{
+    return Dual(x.real() + y.real(), x.infsimal() + y.infsimal());
+}
+
+Dual operator*(const Dual &x, const Dual &y)
+{
+    return Dual(x.real() * y.real(), x.real() * y.infsimal() + x.infsimal() * y.real());
+}
+
+Dual func_to_deriv(Dual x)
+{
+    return (x + Dual(2.0, 0.0)) * (x + Dual(1.0, 0.0));
+}
+
+int main()
+{
+    Dual d(0.0, 1.0);
+    std::cout<<func_to_deriv(Dual(3.0) + d).deriv()<<std::endl;
+    return 0;
+};
